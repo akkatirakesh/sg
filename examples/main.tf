10e3270a7c116472d6065a48dd2a10ef2b2d3c52
akkatirakesh <rakesh-reddy.akkati@mnsu.edu>
2020-09-16 10:57:03 -0500

    modified

--- examples/main.tf
+++ examples/main.tf
@@ -4,12 +4,6 @@ module "subnet" {
   cidr_blocks = var.cidr
 }
 
-provider "aws" {
-  access_key="AKIAQAKEBHAJL5U4HOQG"
-  secret_key="nOsoGIl8oEOf3w1dZPNp/YuzN6FERq5yJZ9QZPHc"
-  region = "us-east-1"
-}
-
 variable "vpcid" {
   default = "vpc-93de4ce9"
 }
