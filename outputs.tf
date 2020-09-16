0c1c355004044ac839f3fa9699ef3c3c2fb5777b
akkatirakesh <rakesh-reddy.akkati@mnsu.edu>
2020-09-16 11:14:43 -0500

    modified outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "name" {
-  value = aws_security_group.mysg.name
-  description="It gives the name of the security group"
+  value       = aws_security_group.mysg.name
+  description = "It gives the name of the security group"
 }
 output "id" {
-  value = aws_security_group.mysg.id
-  description="It gives the security group id"
+  value       = aws_security_group.mysg.id
+  description = "It gives the security group id"
 }
 output "arn" {
-  value = aws_security_group.mysg.arn
-  description="It gives the security group arn"
+  value       = aws_security_group.mysg.arn
+  description = "It gives the security group arn"
 }
 output "description" {
-  value = aws_security_group.mysg.description
-  description="It gives the security group description"
+  value       = aws_security_group.mysg.description
+  description = "It gives the security group description"
 }
